f37050679ff6c3aa1909a767298e6147f869bf23
Svetlana <nooch@yandex.ru>
2023-02-19 21:45:31 +0300

    вывод треугольного числа

--- ext01.java
+++ ext01.java
@@ -12,14 +12,16 @@ public class ext01 {
         System.out.println("Введите число- ");
         int a = iScanner.nextInt();
         iScanner.close();
+        System.out.println("Треугольное число = " + Triangle(a) + "\n");
     }
     
-
-
     public static int Triangle(int num) {
         if (num == 1) {
             return 1;
         }
-        return Triangle(num - 1)+ num;
+        return Triangle(num - 1) + num;
     }
+    
+
+
 }

f8eeee4eb846902939430f64d6df88b395c8ab82
Svetlana <nooch@yandex.ru>
2023-02-19 21:24:29 +0300

    ввод числа

---
+++ ext01.java
@@ -0,0 +1,15 @@
+//Написать программу вычисления n-ого треугольного числа
+
+package homework;
+
+import java.util.Scanner;
+
+public class ext01 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Введите число- ");
+        int a = iScanner.nextInt();
+        iScanner.close();
+    }
+    
+}
